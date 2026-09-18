4cce1626c7e21b89502add8432f5e92a3c9f4c06
Hao Nguyen <hao362k5@gmail.com>
2026-09-18 14:46:49 +0700

    init backend

---
+++ promo-graud-be/src/main/java/group2d/promo_graud/PromoGraudApplication.java
@@ -0,0 +1,13 @@
+package group2d.promo_graud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoGraudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromoGraudApplication.class, args);
+	}
+
+}
---
+++ promo-graud-be/src/test/java/group2d/promo_graud/PromoGraudApplicationTests.java
@@ -0,0 +1,13 @@
+package group2d.promo_graud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromoGraudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
